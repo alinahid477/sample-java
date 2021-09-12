ffc19c54afbb47ac9362ed4366791e25b952209a
Ali Nahid <alinahid@gmail.com>
2021-09-12 15:05:19 +1000

    spring boot app

---
+++ sample-java/demo/src/main/java/com/sample/demo/services/AddService.java
@@ -0,0 +1,5 @@
+package com.sample.demo.services;
+
+public interface AddService {
+    public double add(double x, double y);
+}
---
+++ sample-java/demo/src/main/java/com/sample/demo/services/AddServiceImpl.java
@@ -0,0 +1,13 @@
+package com.sample.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddServiceImpl implements AddService {
+
+    @Override
+    public double add(double x, double y) {
+        return x + y;
+    }
+    
+}
